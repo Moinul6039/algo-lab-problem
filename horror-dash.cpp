b450f32ad134c54c7ab25dc40390abde8d9256c6
MD MOINUL HOSSAIN <hossain15-6039@diu.edu.bd>
2023-09-28 18:46:16 +0600

    algo

---
+++ horror-dash.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int T, N, current, max;
+
+    scanf("%d", &T);
+
+    for (int t = 1; t <= T; ++t)
+    {
+        max = 1;
+
+        scanf("%d", &   N);
+
+        while (N--)
+        {
+            scanf("%d", &current);
+
+            if (max < current)
+                max = current;
+
+        }
+
+        printf("Case %d: %d\n", t, max);
+    }
+
+
+
+}
